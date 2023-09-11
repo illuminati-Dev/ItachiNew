c0fb57caf3f36d87ce7861fbcd8b2fa5aff23e26
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-09-11 21:55:25 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
 
     API_ID = 4042547  # integer value, dont use ""
     API_HASH = "26edf5e69b62da2b6c4e3b8336e22f34"
-    TOKEN = "1979449216:AAEWAmR-m5PkmMLnUJYy3oaD5dABM0n2WbM"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5743243857  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Theharxhu"
     SUPPORT_CHAT = 'Makima_Bot_Support'  # Your own group for support, do not add the @
@@ -25,13 +25,13 @@ class Config(object):
     ERROR_LOGS = 1476517140  # gives alerts about errors u can kepp same as support chat or even OWNER_ID(bot will give errors personally).
 
     # RECOMMENDED
-    DATABASE_URL = 'postgresql://makima:makima1234@132.145.204.100:5432/makima'  # needed for any database modules
+    DATABASE_URL = ''  # needed for any database modules
     LOAD = []
     NO_LOAD = ['cleaner', 'connection']
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "MNo3lF_tu8hYL1sojQi~F5fdMEhKwDwVvPFmx0Fd9B6DovG74fU0U2sBhpRamK2J"  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -60,8 +60,8 @@ class Config(object):
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
-    REDIS_URL = 'redis://Godanuj:Godanuj_123@redis-15994.c12.us-east-1-4.ec2.cloud.redislabs.com:15994/Godanuj'
-    MONGO_DB_URI = 'mongodb+srv://makima:makima@cluster0.fid6i.mongodb.net/cluster0?retryWrites=true&w=majority'
+    REDIS_URL = ''
+    MONGO_DB_URI = ''
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     STRICT_GMUTE = True
@@ -71,7 +71,7 @@ class Config(object):
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = '3OSIXMABP4OEWO65'  # Get your API key from https://www.alphavantage.co/support/#api-key
     TIME_API_KEY = 'Z88KT2UGX31O'  # Get your API key from https://timezonedb.com/api  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    GENIUS = '4wOHQcDEh5gM_XzGU9moT4r--IWyiqVogo1V3SLNcn0q2G8ksjzKR0JJDa8lTHcA'
+    GENIUS = ''
     REM_BG_API_KEY = 'R9Je1GEfhiwjq8X4uswJve3U'
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
@@ -80,8 +80,8 @@ class Config(object):
     ARQ_API_URL = "https://arq.hamker.dev"
     BOT_ID = 1979449216
     BOT_USERNAME = "@makima_ultraxbot"
-    ANI_DB = "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    CHAT_DB = "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    ANI_DB = ""
+    CHAT_DB = ""
     ALLOW_CHATS = True
     DOWN_PATH = "anibot/downloads/"
     KUKI_TOKEN = "ce95970e-8ff5-4ddf-9710-e5fbd54a78f5"
