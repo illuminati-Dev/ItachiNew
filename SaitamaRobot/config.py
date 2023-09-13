38e123ddf705799ba331c1052cb8f5fe7869e768
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-13 15:58:55 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -57,7 +57,7 @@ class Config(object):
     ]
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
-    PORT = 8080
+    PORT = 127.0.0.1
     REDIS_URL = 'redis://default:neko69@redis-18084.c289.us-west-1-2.ec2.cloud.redislabs.com:18084/Neko-Free-db'
     MONGO_DB_URI = 'mongodb+srv://vortex:yNNrzMsR0BAiI4iY@cluster0.8sizo.mongodb.net/?retryWrites=true&w=majorityy'
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
