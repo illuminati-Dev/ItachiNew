eedfb02d4b98286788895673a918c0ea977448de
illuminati-Dev <148691869+illuminati-Dev@users.noreply.github.com>
2023-10-24 21:22:39 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -59,7 +59,7 @@ class Config(object):
     CERT_PATH = None
     PORT = 5000
     REDIS_URL = 'redis://default:neko69@redis-18084.c289.us-west-1-2.ec2.cloud.redislabs.com:18084/Neko-Free-db'
-    MONGO_DB_URI = 'mongodb+srv://BlackHatDev:BlackHatDev@blackhatdev.zk92igo.mongodb.net/?retryWrites=true&w=majority'
+    MONGO_DB_URI = ''
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
     STRICT_GMUTE = True
