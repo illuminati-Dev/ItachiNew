fa2410b788ab78426634baf018b1956c18f18eea
illuminati-Dev <148691869+illuminati-Dev@users.noreply.github.com>
2023-10-24 21:28:52 +0530

    Update db.py

--- SaitamaRobot/utlis/db.py
+++ SaitamaRobot/utlis/db.py
@@ -1,20 +1,19 @@
-# Import necessary modules
 import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.core import AgnosticClient, AgnosticDatabase, AgnosticCollection
-from SaitamaRobot import ANI_DB
 
 # Define the list of symbols to export
-__all__ = ['get_collection']
+__all__ = ['get_collection', 'close_db']
 
 # Establish a connection to the MongoDB server
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(ANI_DB)
 
 # Function to check if the "Makima" database exists and create it if not
 async def check_and_create_database():
-    if "Makima" not in await _MGCLIENT.list_database_names():
-        print("Makima Database Not Found :( => Creating New Database...")
-        await _MGCLIENT["Makima"].list_collection_names()
+    database_name = "Makima"
+    if database_name not in await _MGCLIENT.list_database_names():
+        print(f"{database_name} Database Not Found :( => Creating New Database...")
+        await _MGCLIENT[database_name].list_collection_names()
 
 # Run the database check and creation function
 asyncio.get_event_loop().run_until_complete(check_and_create_database())
@@ -28,7 +27,7 @@ def get_collection(name: str) -> AgnosticCollection:
     return _DATABASE[name]
 
 # Function to close the MongoDB client
-def _close_db() -> None:
+def close_db() -> None:
     _MGCLIENT.close()
 
 # Usage example:
@@ -36,4 +35,4 @@ collection = get_collection("Makima")
 # Perform database operations using the "collection" object
 
 # To close the MongoDB client when done:
-_close_db()
+close_db()
