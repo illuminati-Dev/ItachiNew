dcdd23756b8bb0cfdfe608077e3b9a0ae246112f
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-10-29 06:58:58 +0530

    Update events.py

--- SaitamaRobot/events.py
+++ SaitamaRobot/events.py
@@ -2,17 +2,16 @@ from telethon import events
 from telethon.tl import *
 from SaitamaRobot import telethn
 
-
 def register(**args):
-    """ Registers a new message. """
-    pattern = args.get('pattern', None)
+    """Registers a new message."""
+    pattern = args.get("pattern")
 
-    r_pattern = r'^[/!]'
+    r_pattern = r"^[/!]"
 
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = f"(?i){pattern}"
 
-    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
 
     def decorator(func):
         telethn.add_event_handler(func, events.NewMessage(**args))
@@ -22,7 +21,8 @@ def register(**args):
 
 
 def chataction(**args):
-    """ Registers chat actions. """
+    """Registers chat actions."""
+
     def decorator(func):
         telethn.add_event_handler(func, events.ChatAction(**args))
         return func
@@ -31,7 +31,8 @@ def chataction(**args):
 
 
 def userupdate(**args):
-    """ Registers user updates. """
+    """Registers user updates."""
+
     def decorator(func):
         telethn.add_event_handler(func, events.UserUpdate(**args))
         return func
@@ -40,11 +41,11 @@ def userupdate(**args):
 
 
 def inlinequery(**args):
-    """ Registers inline query. """
-    pattern = args.get('pattern', None)
+    """Registers inline query."""
+    pattern = args.get("pattern")
 
-    if pattern is not None and not pattern.startswith('(?i)'):
-        args['pattern'] = '(?i)' + pattern
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = f"(?i){pattern}"
 
     def decorator(func):
         telethn.add_event_handler(func, events.InlineQuery(**args))
@@ -54,76 +55,8 @@ def inlinequery(**args):
 
 
 def callbackquery(**args):
-    """ Registers inline query. """
+    """Registers inline query."""
+
     def decorator(func):
         telethn.add_event_handler(func, events.CallbackQuery(**args))
         return func
-
-    return decorator
-
-async def check_permissions(event, user_id, p="", cb=False):
-    try:
-        pp = await event.client(
-            functions.channels.GetParticipantRequest(e.chat_id, user_id)
-        )
-    except:
-        return False
-    pp = pp.participant
-    if isinstance(pp, types.ChannelParticipant):
-        if cb:
-            await event.answer("You should be admin to-do it!")
-        else:
-            await event.reply("You should be admin to-do it!")
-        return False
-    elif isinstance(pp, types.ChannelParticipantCreator):
-        return True
-    elif isinstance(pp, types.ChannelParticipantAdmin):
-        if p == "add_admins":
-            if pp.admin_rights.add_admins:
-                return True
-            if cb:
-                await event.answer(
-                    "You are missing the following rights to use this command: CanPromoteUsers.",
-                    alert=True,
-                )
-            else:
-                await event.reply(
-                    "You are missing the following rights to use this command: CanPromoteUsers."
-                )
-        elif p == "ban_users":
-            if pp.admin_rights.ban_users:
-                return True
-            if cb:
-                await event.answer(
-                    "You are missing the following rights to use this command: CanRestrictMembers.",
-                    alert=True,
-                )
-            else:
-                await event.reply(
-                    "You are missing the following rights to use this command: CanRestrictMembers."
-                )
-        elif p == "change_info":
-            if pp.admin_rights.change_info:
-                return True
-            if cb:
-                await event.answer(
-                    "You are missing the following rights to use this command: CanChangeInfo.",
-                    alert=True,
-                )
-            else:
-                await event.reply(
-                    "You are missing the following rights to use this command: CanChangeInfo."
-                )
-        elif p == "delete_messages":
-            if pp.admin_rights.delete_messages:
-                return True
-            if cb:
-                await event.answer(
-                    "You are missing the following rights to use this command: CanDeleteMessages.",
-                    alert=True,
-                )
-            else:
-                await event.reply(
-                    "You are missing the following rights to use this command: CanDeleteMessages."
-                )
-        return False
