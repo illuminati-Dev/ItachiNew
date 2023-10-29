461b8959b8d95e8a34642d1fc01e4bf662bf8a4d
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-10-29 06:52:13 +0530

    Update speed_test.py

--- SaitamaRobot/modules/speed_test.py
+++ SaitamaRobot/modules/speed_test.py
@@ -6,53 +6,64 @@ from SaitamaRobot import DEV_USERS, dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
 
+
 def convert(speed):
     return round(int(speed) / 1048576, 2)
+
 
 @dev_plus
 def speedtestxyz(update: Update, context: CallbackContext):
     buttons = [
         [
-            InlineKeyboardButton("Image", callback_data="speedtest_image"),
-            InlineKeyboardButton("Text", callback_data="speedtest_text")
-        ]
+            InlineKeyboardButton("ɪᴍᴀɢᴇ", callback_data="speedtest_image"),
+            InlineKeyboardButton("ᴛᴇxᴛ", callback_data="speedtest_text"),
+        ],
     ]
     update.effective_message.reply_text(
-        "Select SpeedTest Mode", reply_markup=InlineKeyboardMarkup(buttons))
+        "sᴇʟᴇᴄᴛ sᴘᴇᴇᴅᴛᴇsᴛ ᴍᴏᴅᴇ",
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+
 
 def speedtestxyz_callback(update: Update, context: CallbackContext):
     query = update.callback_query
 
     if query.from_user.id in DEV_USERS:
-        msg = update.effective_message.edit_text('Running a speedtest....')
+        msg = update.effective_message.edit_text("ʀᴜɴɴɪɴɢ ғᴏʀ sᴘᴇᴇᴅᴛᴇsᴛ....")
         speed = speedtest.Speedtest()
         speed.get_best_server()
         speed.download()
         speed.upload()
-        replymsg = 'SpeedTest Results:'
+        replymsg = "sᴘᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs:"
 
-        if query.data == 'speedtest_image':
+        if query.data == "speedtest_image":
             speedtest_image = speed.results.share()
             update.effective_message.reply_photo(
-                photo=speedtest_image, caption=replymsg)
+                photo=speedtest_image,
+                caption=replymsg,
+            )
             msg.delete()
 
-        elif query.data == 'speedtest_text':
+        elif query.data == "speedtest_text":
             result = speed.results.dict()
-            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
-            update.effective_message.edit_text(
-                replymsg, parse_mode=ParseMode.MARKDOWN)
+            replymsg += f"\nᴅᴏᴡɴʟᴏᴀᴅ: `{convert(result['download'])}Mb/s`\nᴜᴘʟᴏᴀᴅ: `{convert(result['upload'])}ᴍʙ/s`\nᴘɪɴɢ: `{result['ping']}`"
+            update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.answer(
-            "You are required to join Heroes Association to use this command.")
+        query.answer("ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜɪʀᴇᴅ ᴛᴏ ᴊᴏɪɴ @{SUPPORT_CHAT} ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
 
-SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz, run_async=True)
+
+SPEED_TEST_HANDLER = DisableAbleCommandHandler(
+    "speedtest", speedtestxyz, run_async=True
+)
 SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
-    speedtestxyz_callback, pattern='speedtest_.*', run_async=True)
+    speedtestxyz_callback,
+    pattern="speedtest_.*",
+    run_async=True,
+)
 
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "SpeedTest"
+__mod_name__ = "𝚂ᴘᴇᴇᴅᴛᴇsᴛ"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
