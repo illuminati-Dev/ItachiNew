9fa80c980f442c9935c0a824701d5cfb2676abb5
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-18 21:54:27 +0530

    Update couples_mongo.py

--- SaitamaRobot/modules/mongo/couples_mongo.py
+++ SaitamaRobot/modules/mongo/couples_mongo.py
@@ -1,26 +1,20 @@
 from SaitamaRobot import db
 from typing import Dict, List, Union
 
-
 coupledb = db.couple
 
-async def _get_lovers(chat_id: int):
+async def get_lovers(chat_id: int) -> Dict[str, dict]:
     lovers = await coupledb.find_one({"chat_id": chat_id})
-    if not lovers:
-        return {}
-    return lovers["couple"]
+    return lovers.get("couple", {})
 
-
-async def get_couple(chat_id: int, date: str):
-    lovers = await _get_lovers(chat_id)
-    if date in lovers:
-        return lovers[date]
-    return False
-
+async def get_couple(chat_id: int, date: str) -> Union[bool, dict]:
+    lovers = await get_lovers(chat_id)
+    return lovers.get(date, False)
 
 async def save_couple(chat_id: int, date: str, couple: dict):
-    lovers = await _get_lovers(chat_id)
-    lovers[date] = couple
-    await coupledb.update_one(
-        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
-    )
+    async with coupledb:
+        await coupledb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {f"couple.{date}": couple}},
+            upsert=True
+        )
