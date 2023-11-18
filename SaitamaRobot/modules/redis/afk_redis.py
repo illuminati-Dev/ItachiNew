fadc24c28ac46dd39b72e1dd349fc8e198838712
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-18 21:52:13 +0530

    Update afk_redis.py

--- SaitamaRobot/modules/redis/afk_redis.py
+++ SaitamaRobot/modules/redis/afk_redis.py
@@ -1,25 +1,68 @@
 from SaitamaRobot import REDIS
 
-
 # AFK
 def is_user_afk(userid):
-    rget = REDIS.get(f"is_afk_{userid}")
-    return bool(rget)
+    """
+    Check if a user is marked as AFK.
 
+    Args:
+        userid (int): User ID.
+
+    Returns:
+        bool: True if the user is AFK, False otherwise.
+    """
+    return bool(REDIS.get(f"is_afk_{userid}"))
 
 def start_afk(userid, reason):
+    """
+    Mark a user as AFK.
+
+    Args:
+        userid (int): User ID.
+        reason (str): AFK reason.
+
+    Returns:
+        None
+    """
     REDIS.set(f"is_afk_{userid}", reason)
 
-
 def afk_reason(userid):
-    return strb(REDIS.get(f"is_afk_{userid}"))
+    """
+    Get the AFK reason for a user.
 
+    Args:
+        userid (int): User ID.
+
+    Returns:
+        str: AFK reason or None if the user is not AFK.
+    """
+    return REDIS.get(f"is_afk_{userid}").decode('utf-8') if is_user_afk(userid) else None
 
 def end_afk(userid):
-    REDIS.delete(f"is_afk_{userid}")
-    return True
+    """
+    End a user's AFK status.
 
+    Args:
+        userid (int): User ID.
+
+    Returns:
+        bool: True if the user was AFK and is now marked as not AFK, False otherwise.
+    """
+    key = f"is_afk_{userid}"
+    if is_user_afk(userid):
+        REDIS.delete(key)
+        return True
+    return False
 
 # Helpers
 def strb(redis_string):
-    return str(redis_string)
+    """
+    Convert a Redis byte string to a decoded string.
+
+    Args:
+        redis_string (bytes): Redis byte string.
+
+    Returns:
+        str: Decoded string or None if the input is None.
+    """
+    return redis_string.decode('utf-8') if redis_string else None
