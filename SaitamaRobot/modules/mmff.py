b7f7d38e0e3d317dcbacc029ada86d32bf3bfcb9
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-11-18 22:19:37 +0530

    Update mmff.py

--- SaitamaRobot/modules/mmff.py
+++ SaitamaRobot/modules/mmff.py
@@ -7,10 +7,11 @@ from SaitamaRobot import TEMP_DOWNLOAD_DIRECTORY
 from SaitamaRobot import pbot as app
 
 # Load a font that supports getsize
-font = ImageFont.truetype("SaitamaRobot/resources/American Captain.ttf", size=14)
+font_path = "SaitamaRobot/resources/American Captain.ttf"
+font_size = 14
+font = ImageFont.truetype(font_path, size=font_size)
 
-# how a lazy guy ports.
-@app.on_message(filters.command("mmf") & filters.reply)
+@register(filters.command("mmf") & filters.reply)
 async def mmf_func(_, message):
     if not message.reply_to_message:
         await message.reply("Reply to a sticker/image with meme text.")
@@ -53,39 +54,29 @@ async def add_text_img(image_path, text):
     top_lines = textwrap.wrap(upper_text, width=chars_per_line)
     bottom_lines = textwrap.wrap(lower_text, width=chars_per_line)
 
-    if top_lines:
-        y = 10
-        for line in top_lines:
-            line_width, line_height = font.getsize(line)
-            x = (image_width - line_width) / 2
-            draw.text(
-                (x, y),
-                line,
-                fill="white",
-                font=font,
-                stroke_width=stroke_width,
-                stroke_fill="black",
-            )
-            y += line_height
-
-    if bottom_lines:
-        y = image_height - char_height * len(bottom_lines) - 15
-        for line in bottom_lines:
-            line_width, line_height = font.getsize(line)
-            x = (image_width - line_width) / 2
-            draw.text(
-                (x, y),
-                line,
-                fill="white",
-                font=font,
-                stroke_width=stroke_width,
-                stroke_fill="black",
-            )
-            y += line_height
+    draw_text_lines(draw, top_lines, char_height, image_width, stroke_width, "white", "black")
+    draw_text_lines(draw, bottom_lines, image_height, image_width, stroke_width, "white", "black", bottom=True)
 
     final_image = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "memify.webp")
     img.save(final_image, **img_info)
     return final_image
+
+def draw_text_lines(draw, lines, y, image_width, stroke_width, text_fill, stroke_fill, bottom=False):
+    for line in lines:
+        line_width, line_height = font.getsize(line)
+        x = (image_width - line_width) / 2
+        if bottom:
+            y -= line_height
+        draw.text(
+            (x, y),
+            line,
+            fill=text_fill,
+            font=font,
+            stroke_width=stroke_width,
+            stroke_fill=stroke_fill,
+        )
+        if not bottom:
+            y += line_height
 
 __help__ = """
 *Commands:* 
