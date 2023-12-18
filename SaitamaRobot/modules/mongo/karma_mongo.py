44f92a299d330267f205967ff4986f74f018723f
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-18 22:19:05 +0530

    Update karma_mongo.py

--- SaitamaRobot/modules/mongo/karma_mongo.py
+++ SaitamaRobot/modules/mongo/karma_mongo.py
@@ -9,22 +9,19 @@ async def get_karmas_count() -> Dict[str, int]:
     karmas_count = 0
 
     async for chat in karmadb.find({"chat_id": {"$lt": 0}}):
-        karmas_count += sum(entry["karma"] for entry in chat["karma"].values() if entry["karma"] > 0)
+        karmas_count += sum(entry["karma"] for entry in chat.get("karma", {}).values() if entry.get("karma", 0) > 0)
         chats_count += 1
 
     return {"chats_count": chats_count, "karmas_count": karmas_count}
 
-
 async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
-    return karma["karma"] if karma else {}
-
+    karma_entry = await karmadb.find_one({"chat_id": chat_id})
+    return karma_entry.get("karma", {}) if karma_entry else {}
 
 async def get_karma(chat_id: int, name: str) -> Union[bool, int]:
     name = name.lower().strip()
     karmas = await get_karmas(chat_id)
     return karmas.get(name, False)
-
 
 async def update_karma(chat_id: int, name: str, karma: int):
     name = name.lower().strip()
@@ -35,11 +32,9 @@ async def update_karma(chat_id: int, name: str, karma: int):
             upsert=True
         )
 
-
 async def is_karma_on(chat_id: int) -> bool:
-    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
-    return bool(chat)
-
+    chat_entry = await karmadb.find_one({"chat_id_toggle": chat_id})
+    return bool(chat_entry)
 
 async def karma_on(chat_id: int):
     is_karma = await is_karma_on(chat_id)
@@ -48,7 +43,6 @@ async def karma_on(chat_id: int):
     async with karmadb:
         await karmadb.delete_one({"chat_id_toggle": chat_id})
 
-
 async def karma_off(chat_id: int):
     is_karma = await is_karma_on(chat_id)
     if not is_karma:
@@ -56,12 +50,10 @@ async def karma_off(chat_id: int):
     async with karmadb:
         await karmadb.insert_one({"chat_id_toggle": chat_id})
 
-
 async def int_to_alpha(user_id: int) -> str:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     return "".join(alphabet[int(i)] for i in str(user_id))
 
-
 async def alpha_to_int(user_id_alphabet: str) -> int:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     return int("".join(str(alphabet.index(i)) for i in user_id_alphabet))
