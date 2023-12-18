5690baba615bca79b7d13e8bad09945967de2fe6
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-18 22:20:08 +0530

    Update chatbot_db.py

--- SaitamaRobot/modules/mongo/chatbot_db.py
+++ SaitamaRobot/modules/mongo/chatbot_db.py
@@ -8,23 +8,24 @@ class Chat:
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
+    @staticmethod
     def is_ai_chat(chat_id):
-        if cha_db.find_one({"chat_id": chat_id}):
-            return True
-        else:
-            return False
+        return cha_db.find_one({"chat_id": chat_id}) is not None
 
+    @staticmethod
     def add_ai(chat_id):
         if not Chat.is_ai_chat(chat_id):
             cha_db.insert_one({"chat_id": chat_id})
         else:
             return
 
+    @staticmethod
     def rm_ai(chat_id):
         if Chat.is_ai_chat(chat_id):
             cha_db.delete_one({"chat_id": chat_id})
         else:
             return
 
+    @staticmethod
     def get_ai_chats():
         return cha_db.find()
