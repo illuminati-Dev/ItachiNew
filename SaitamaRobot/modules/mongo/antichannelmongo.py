684ef4f74b993ed2d31cf5358e46e4f026407113
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-18 22:06:07 +0530

    Update antichannelmongo.py

--- SaitamaRobot/modules/mongo/antichannelmongo.py
+++ SaitamaRobot/modules/mongo/antichannelmongo.py
@@ -1,25 +1,16 @@
 from SaitamaRobot import CHAT_DB
 from pymongo import MongoClient
 
+# Initialize MongoDB client and collection
 antichanneldb = MongoClient(CHAT_DB)["KUKI"]["antichannel"]
 
 def antichannel_status(chat_id):
-    is_achannel = antichanneldb.find_one({"chat_id": chat_id})
-    if not is_achannel:
-        return False
-    else:
-        return True
+    return antichanneldb.find_one({"chat_id": chat_id}) is not None
 
 def enable_antichannel(chat_id):
-    is_achannel = antichannel_status(chat_id)
-    if is_achannel:
-        return
-    else:
-        return antichanneldb.insert_one({"chat_id": chat_id})
+    if not antichannel_status(chat_id):
+        antichanneldb.insert_one({"chat_id": chat_id})
 
 def disable_antichannel(chat_id):
-    is_achannel = antichannel_status(chat_id)
-    if not is_achannel:
-        return
-    else:
-        return antichanneldb.delete_one({"chat_id": chat_id})
+    if antichannel_status(chat_id):
+        antichanneldb.delete_one({"chat_id": chat_id})
