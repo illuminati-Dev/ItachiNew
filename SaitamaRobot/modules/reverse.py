b6d52171e9ca7d376b02c828c55f936f712dc491
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-18 21:34:52 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -1,121 +1,99 @@
+from typing import BinaryIO, Dict, List
+import time
 import os
-import re
-import requests
-import urllib
-import urllib.request
-import urllib.parse
-from urllib.error import URLError, HTTPError
-from bs4 import BeautifulSoup
-from pyrogram import filters
-from pyrogram.types import InputMediaPhoto
-from SaitamaRobot import pbot as app
+from uuid import uuid4
 
-opener = urllib.request.build_opener()
-useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
-opener.addheaders = [('User-agent', useragent)]
+import httpx
 
-@app.on_message(filters.command(["reverse", "grs", "pp"]))
-async def reverse(_, message):
-    if os.path.isfile("okgoogle.png"):
-        os.remove("okgoogle.png")
+from pyrogram.enums import MessageMediaType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram import Client, filters
 
-    chat_id = message.chat.id
-    rtmid = message.message_id
-    imagename = "okgoogle.png"
+# Replace `YourRobot` with your robot and import pyrogram client from it.
+from Madara import pgram as app
 
-    reply = message.reply_to_message
-    if reply:
-        if reply.sticker:
-            file_id = reply.sticker.file_id
-        elif reply.photo:
-            file_id = reply.photo[-1].file_id
-        elif reply.document:
-            file_id = reply.document.file_id
-        else:
-            await message.reply_text("Reply to an image or sticker to look it up.")
-            return
-        image_file = await app.get_file(file_id)
-        await image_file.download(imagename)
-    elif message.command:
-        splatargs = message.text.split(" ")
-        if len(splatargs) == 3:
-            img_link = splatargs[1]
-            try:
-                lim = int(splatargs[2])
-            except:
-                lim = 2
-        elif len(splatargs) == 2:
-            img_link = splatargs[1]
-            lim = 2
-        else:
-            await message.reply_text("/reverse <link> <amount of images to return>")
-            return
-        try:
-            urllib.request.urlretrieve(img_link, imagename)
-        except HTTPError as HE:
-            if HE.reason == 'Not Found':
-                await message.reply_text("Image not found.")
-                return
-            elif HE.reason == 'Forbidden':
-                await message.reply_text("Couldn't access the provided link. The website might have blocked access by the bot or the website does not exist.")
-                return
-        except URLError as UE:
-            await message.reply_text(f"{UE.reason}")
-            return
-        except ValueError as VE:
-            await message.reply_text(f"{VE}\nPlease try again using http or https protocol.")
-            return
-    else:
-        await message.reply_markdown("Please reply to a sticker or an image to search it. You can also search an image with a link using `/reverse [picturelink] <amount>`.")
+API_URL: str = "https://sasta-api.vercel.app/googleImageSearch"
+
+COMMANDS: List[str] = [
+    "reverse",
+    "grs",
+    "pp"
+]
+
+async_client: httpx.AsyncClient = httpx.AsyncClient(timeout=120)
+
+class STRINGS:
+    REPLY_TO_MEDIA: str = "📸 Please reply to a message that contains one of the supported media types, such as a photo, sticker, or image file."
+    UNSUPPORTED_MEDIA_TYPE: str = "⚠️ <b>Unsupported media type!</b>\n📸 Please reply with a supported media type: image, sticker, or image file."
+
+    DOWNLOADING_MEDIA: str = "⏳ Downloading media..."
+    UPLOADING_TO_API_SERVER: str = "🚀 Uploading media to <b>API Server</b>... 📡"
+    PARSING_RESULT: str = "💻 Parsing result..."
+
+    EXCEPTION_OCCURRED: str = "❌ <b>Exception occurred!</b>\n\n<b>Exception:</b> {}"
+
+    RESULT: str = """
+🔤 <b>Query:</b> <code>{query}</code>
+🔗 <b>Page Link:</b> <a href="{page_url}">Link</a>
+
+⌛️ <b>Time Taken:</b> <code>{time_taken}</code> seconds.
+    """
+    OPEN_PAGE: str = "↗️ Open Page"
+
+@app.on_message(filters.command(COMMANDS))
+async def on_reverse(client: Client, message: Message) -> None:
+    if not message.reply_to_message:
+        await message.reply(STRINGS.REPLY_TO_MEDIA)
+        return
+    elif message.reply_to_message.media not in (MessageMediaType.PHOTO, MessageMediaType.STICKER, MessageMediaType.DOCUMENT):
+        await message.reply(STRINGS.UNSUPPORTED_MEDIA_TYPE)
         return
 
+    start_time: float = time.time()
+    status_msg: Message = await message.reply(STRINGS.DOWNLOADING_MEDIA)
+    file_path: str = f"temp_download/{uuid4()}"
     try:
-        searchUrl = 'https://www.google.com/searchbyimage/upload'
-        multipart = {'encoded_image': (imagename, open(imagename, 'rb')), 'image_content': ''}
-        response = requests.post(searchUrl, files=multipart, allow_redirects=False)
-        fetchUrl = response.headers['Location']
+        await message.reply_to_message.download(file_path)
+    except Exception as exc:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(exc)
+        await message.reply(text)
+        try:
+            os.remove(file_path)
+        except FileNotFoundError:
+            pass
+        return
 
-        if response.status_code != 400:
-            xx = await app.send_message(chat_id, "Image was successfully uploaded to Google. Parsing it, please wait.", reply_to_message_id=rtmid)
-        else:
-            xx = await app.send_message(chat_id, "Google told me to go away.", reply_to_message_id=rtmid)
-            return
+    await status_msg.edit(STRINGS.UPLOADING_TO_API_SERVER)
+    files: Dict[str, BinaryIO] = {"file": open(file_path, "rb")}
+    response: httpx.Response = await async_client.post(API_URL, files=files)
+    os.remove(file_path)
 
-        os.remove(imagename)
-        match = ParseSauce(fetchUrl + "&hl=en")
-        guess = match['best_guess']
-        if match['override'] and not match['override'] == '':
-            imgspage = match['override']
-        else:
-            imgspage = match['similar_images']
+    if response.status_code == 404:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(response.json()["error"])
+        await message.reply(text)
+        await status_msg.delete()
+        return
+    elif response.status_code != 200:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(response.text)
+        await message.reply(text)
+        await status_msg.delete()
+        return
 
-        if guess and imgspage:
-            await xx.edit_text(f"[{guess}]({fetchUrl})\nProcessing...", parse_mode='markdown', disable_web_page_preview=True)
-        else:
-            await xx.edit_text("Couldn't find anything.")
-            return
+    await status_msg.edit(STRINGS.PARSING_RESULT)
+    response_json: Dict[str, str] = response.json()
+    query: str = response_json["query"]
+    page_url: str = response_json["url"]
 
-        images = scam(imgspage, lim)
-        if len(images) == 0:
-            await xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})\nCouldn't fetch any images.", parse_mode='markdown', disable_web_page_preview=True)
-            return
+    end_time: float = time.time() - start_time
+    time_taken: str = "{:.2f}".format(end_time)
 
-        imglinks = []
-        for link in images:
-            lmao = InputMediaPhoto(media=str(link))
-            imglinks.append(lmao)
-
-        await app.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
-        await xx.edit_text(f"[{guess}]({fetchUrl})\n[Visually similar images]({imgspage})", parse_mode='markdown', disable_web_page_preview=True)
-    except Exception as e:
-        print(e)
-    except Exception as exception:
-        print(exception)
-
-__help__ = """
-*Commands:* 
-• /reverse | pp | grs: Does a reverse image search of the media which it was replied to.
-Reports bugs at @UchihaPolice_Support
-"""
-
-__mod_name__ = "Reverse"
+    text: str = STRINGS.RESULT.format(
+        query=query,
+        page_url=page_url,
+        time_taken=time_taken
+    )
+    buttons: List[List[InlineKeyboardButton]] = [
+        [InlineKeyboardButton(STRINGS.OPEN_PAGE, url=page_url)]
+    ]
+    await message.reply(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
+    await status_msg.delete()
