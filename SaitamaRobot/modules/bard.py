f9852112bc2136cada0458440f205f1a19f86d73
Harshal <148691869+illuminati-Dev@users.noreply.github.com>
2023-12-19 10:45:20 +0530

    Update bard.py

--- SaitamaRobot/modules/bard.py
+++ SaitamaRobot/modules/bard.py
@@ -3,12 +3,16 @@
 # PROVIDED BY https://t.me/ProjectCodeX
 
 import requests
-from pyrogram import filters,types
+from pyrogram import filters, types
 from SaitamaRobot import pbot as app
-# example: from Boa import app
+
+__mod_name__ = "Bard"
+__help__ = """
+- `/bard [prompt]`: Generate content based on the provided prompt using the Bard model.
+"""
 
 @app.on_message(filters.command("bard"))
-async def bard(client,message):
+async def bard(client, message):
     prompt = None
     if message.text is None:
         prompt = None
@@ -16,30 +20,30 @@ async def bard(client,message):
         try:
             return message.text.split(None, 1)[1]
         except IndexError:
-            prompt =  None
+            prompt = None
     else:
         prompt = None
     if prompt is None:
-        return await message.reply_text("Hello, How can i assist you today?")
+        return await message.reply_text("Hello, how can I assist you today?")
     resp = requests.post(
         f"https://lexica.qewertyy.me/models?model_id=20&prompt={prompt}"
-        )
+    )
     if resp.status_code != 200:
-        return await message.reply_text("An error occured.")
+        return await message.reply_text("An error occurred.")
     data = resp.json()
     try:
-        text,images = data['content'], data['images']
+        text, images = data['content'], data['images']
         if len(images) == 0:
             await message.reply_text(text)
         else:
             media = []
             for i in images:
                 media.append(types.InputMediaPhoto(i))
-            media[0] = types.InputMediaPhoto(images[0],caption=text)
+            media[0] = types.InputMediaPhoto(images[0], caption=text)
             await app.send_media_group(
                 message.chat.id,
                 media,
                 reply_to_message_id=message.id
-                )
+            )
     except Exception as Ok:
-        print(Ok,data)
+        print(Ok, data)
